fdc6aec35294d387e417fe220edcfa266c44ed0f
Nick Hinderer <nxhinde@colostate.edu>
2022-04-14 14:40:47 -0600

    removed commented out not needed visits

--- src/main/java/com/company/SymbolTableVisitor.java
+++ src/main/java/com/company/SymbolTableVisitor.java
@@ -3,7 +3,7 @@ package com.company;
 import syntaxtree.*;
 import visitor.GJDepthFirst;
 
-public class SymbolTableVisitor<R,A> extends GJDepthFirst<R,A> {
+public class SymbolTableVisitor<R, A> extends GJDepthFirst<R, A> {
 
 
     private static SymbolTable symbolTable = new SymbolTable();
@@ -12,107 +12,6 @@ public class SymbolTableVisitor<R,A> extends GJDepthFirst<R,A> {
 
     public SymbolTable getSymbolTable() {
         return symbolTable;
-    }
-
-
-
-    @Override
-    public R visit(MainClass n, A/*SymbolTable (and return void)*/ argu) { //come back adn rewrite with new symboltable
-        String className = n.f1.f0.tokenImage;
-        ClassType classType = new ClassType(className, null, true);
-        if (!symbolTable.addClass(className, classType))
-            throw new TypeCheckException("Redefinition of of class '" + className + "'");
-        currentClass = className;
-//        n.f11.accept(this, argu);
-        n.f14.accept(this, argu);
-//        n.f15.accept(this, argu);
-        return null;
-    }
-
-    @Override
-    public R visit(ClassDeclaration n, A argu) {
-        String className = n.f1.f0.tokenImage;
-        ClassType classType = new ClassType(className, null);
-        if (!symbolTable.addClass(className, classType))
-            throw new TypeCheckException("Redefinition of of class '" + className + "'");
-        currentClass = className;
-        R _ret = null;
-//        n.f1.accept(this, argu);
-        field = true;
-        n.f3.accept(this, argu);
-        field = false;
-        n.f4.accept(this, argu);
-        return _ret;
-    }
-
-    @Override
-    public R visit(ClassExtendsDeclaration n, A argu) {
-        R _ret = null;
-        String className = n.f1.f0.tokenImage;
-        String parentName = n.f3.f0.tokenImage;
-        ClassType classType = new ClassType(className, parentName);
-        if (!symbolTable.addClass(className, classType))
-            throw new TypeCheckException("Redefinition of of class '" + className + "'");
-        currentClass = className;
-//        n.f1.accept(this, argu);
-//        n.f3.accept(this, argu);
-        field = true;
-        n.f5.accept(this, argu);
-        field = false;
-        n.f6.accept(this, argu);
-        return _ret;
-    }
-
-    @Override
-    public R visit(MethodDeclaration n, A argu) {
-        R _ret = null;
-        String methodName = n.f2.f0.tokenImage;
-        Type returnType = getType(n.f1.f0);
-        MethodType methodType = new MethodType(returnType);
-        if (!symbolTable.addMethod(currentClass, methodName, methodType))
-            throw new TypeCheckException("Redefinition of of method '" + methodName + "' in class '" + currentClass + "'");
-
-        currentMethod = methodName;
-//        n.f1.accept(this, argu);
-//        n.f2.accept(this, argu);
-        parameter = true;
-        n.f4.accept(this, argu);
-        parameter = false;
-        variable = true;
-        n.f7.accept(this, argu);
-        variable = false;
-//        n.f8.accept(this, argu);
-//        n.f10.accept(this, argu);
-        return _ret;
-    }
-
-    @Override
-    public R visit(VarDeclaration n, A argu) {
-        R _ret=null;
-        Type type = getType(n.f0.f0);
-        String name = n.f1.f0.tokenImage;
-        if (field)
-            if(!symbolTable.addField(currentClass, name, type))
-                throw new TypeCheckException("Redefinition of of field '" + name + "' in class '" + currentClass + "'");
-        if (variable)
-            if (!symbolTable.addVariable(currentClass, currentMethod, name, type))
-                throw new TypeCheckException("Redefinition of variable '" + name + "' in method '" + currentMethod + "' in class '" + currentClass + "'");
-//        n.f0.accept(this, argu);
-//        n.f1.accept(this, argu);
-        return _ret;
-    }
-
-    @Override
-    public R visit(FormalParameter n, A argu) {
-        R _ret=null;
-        Type type = getType(n.f0.f0);
-        String name = n.f1.f0.tokenImage;
-        if (parameter)
-            if (symbolTable.addParameter(currentClass, currentMethod, name, type) == false)
-                throw new TypeCheckException("Redefinition of variable '" + name + "' in method '" + currentMethod + "' in class '" + currentClass + "'");
-//        n.f0.accept(this, argu);
-//        n.f1.accept(this, argu);
-        return _ret;
     }
 
     private static Type getType(NodeChoice n) {
@@ -135,19 +34,84 @@ public class SymbolTableVisitor<R,A> extends GJDepthFirst<R,A> {
         return type;
     }
 
+    @Override
+    public R visit(MainClass n, A argu) { //come back adn rewrite with new symboltable
+        String className = n.f1.f0.tokenImage;
+        ClassType classType = new ClassType(className, null, true);
+        if (!symbolTable.addClass(className, classType))
+            throw new TypeCheckException("Redefinition of of class '" + className + "'");
+        currentClass = className;
+        n.f14.accept(this, argu);
+        return null;
+    }
+
+    @Override
+    public R visit(ClassDeclaration n, A argu) {
+        String className = n.f1.f0.tokenImage;
+        ClassType classType = new ClassType(className, null);
+        if (!symbolTable.addClass(className, classType))
+            throw new TypeCheckException("Redefinition of of class '" + className + "'");
+        currentClass = className;
+        field = true;
+        n.f3.accept(this, argu);
+        field = false;
+        n.f4.accept(this, argu);
+        return null;
+    }
+
+    @Override
+    public R visit(ClassExtendsDeclaration n, A argu) {
+        String className = n.f1.f0.tokenImage;
+        String parentName = n.f3.f0.tokenImage;
+        ClassType classType = new ClassType(className, parentName);
+        if (!symbolTable.addClass(className, classType))
+            throw new TypeCheckException("Redefinition of of class '" + className + "'");
+        currentClass = className;
+        field = true;
+        n.f5.accept(this, argu);
+        field = false;
+        n.f6.accept(this, argu);
+        return null;
+    }
+
+    @Override
+    public R visit(MethodDeclaration n, A argu) {
+        String methodName = n.f2.f0.tokenImage;
+        Type returnType = getType(n.f1.f0);
+        MethodType methodType = new MethodType(returnType);
+        if (!symbolTable.addMethod(currentClass, methodName, methodType))
+            throw new TypeCheckException("Redefinition of of method '" + methodName + "' in class '" + currentClass + "'");
+        currentMethod = methodName;
+        parameter = true;
+        n.f4.accept(this, argu);
+        parameter = false;
+        variable = true;
+        n.f7.accept(this, argu);
+        variable = false;
+        return null;
+    }
+
+    @Override
+    public R visit(VarDeclaration n, A argu) {
+        Type type = getType(n.f0.f0);
+        String name = n.f1.f0.tokenImage;
+        if (field)
+            if (!symbolTable.addField(currentClass, name, type))
+                throw new TypeCheckException("Redefinition of of field '" + name + "' in class '" + currentClass + "'");
+        if (variable)
+            if (!symbolTable.addVariable(currentClass, currentMethod, name, type))
+                throw new TypeCheckException("Redefinition of variable '" + name + "' in method '" + currentMethod + "' in class '" + currentClass + "'");
+        return null;
+    }
+
+    @Override
+    public R visit(FormalParameter n, A argu) {
+        Type type = getType(n.f0.f0);
+        String name = n.f1.f0.tokenImage;
+        if (parameter)
+            if (symbolTable.addParameter(currentClass, currentMethod, name, type) == false)
+                throw new TypeCheckException("Redefinition of variable '" + name + "' in method '" + currentMethod + "' in class '" + currentClass + "'");
+        return null;
+    }
+
 }
-//switch (n.f0.f0.which) {
-//        case 0:
-//        type = new ArrayType();
-//        break;
-//        case 1:
-//        type = new PrimitiveType("boolean");
-//        break;
-//        case 2:
-//        type = new PrimitiveType("int");
-//        break;
-//        case 3:
-//        String className = ((Identifier)n.f0.f0.choice).f0.tokenImage;
-//        type = new ClassType(className, null);
-//        break;
-//        }
